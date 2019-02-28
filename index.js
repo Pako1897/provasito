4fd9ccfe8691890ebe9f42c7d0219a2629a8ee8c
pako1897 <juventuss879@gmail.com>
2019-02-28 19:41:10 +0000

    modifica MaComprami

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res){
   res.sendFile(__dirname+'/public/index.html');
 })
 
+app.get('/smartphone', function(req,res){
+  res.sendFile(__dirname+'/public/smartphone.html');
+})
+
 app.use(function(req,res){
   res.status(404);
   res.sendFile(__dirname+'/public/404.html');
